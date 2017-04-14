7b1ea1551ee5cff3d1168bce4660b17fb59ad779
juliand665 <julian.dunskus@gmail.com>
2017-04-14 16:32:27 +0200

    Skeleton Set Up
    Just the default document-based app project, slightly edited.

---
+++ Colorizer/Colorizer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Colorizer
+//
+//  Created by Julian Dunskus on 14.04.17.
+//  Copyright © 2017 Julian Dunskus. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
